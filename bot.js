ab357181cffd2770b108fa4fe2686c0195a96ecc
impunityBOT <42361041+impunityBOT@users.noreply.github.com>
2018-08-14 02:50:14 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const robot = new Descord.Client();
+
+client.login ("NDc4Njk4Mzg0NzM2MTI0OTM5.DlOeaQ.1X6g6ExVaHx0bGOX4kQpFATSzwE");
+
+client.on('message',message => {
+    if (message.content === 'привет') {
+        message.reply('hi!');
+    }
+});
+
